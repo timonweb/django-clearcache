75c56340790be2718d6d77eb579dfe10b5ff541a
Daniel Pinto <Jigsaw52@users.noreply.github.com>
2022-12-27 01:12:57 +0000

    Add missing closing parentheses after cache backend
    The entries in dropdown to select the cache to clear are missing a closing parentheses after the cache backend. This patch adds it.

--- clearcache/forms.py
+++ clearcache/forms.py
@@ -4,7 +4,7 @@ from django.conf import settings
 
 def get_cache_choices():
     caches = settings.CACHES or {}
-    return [(key, f"{key} ({cache['BACKEND']}") for key, cache in caches.items()]
+    return [(key, f"{key} ({cache['BACKEND']})") for key, cache in caches.items()]
 
 
 class ClearCacheForm(forms.Form):
